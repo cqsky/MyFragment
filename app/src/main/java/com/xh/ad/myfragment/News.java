d3591344175cd3f47bb72685ae7e6073e64b3f99
cqsky <931828906@qq.com>
2018-04-02 22:49:13 +0800

    Initial commit

---
+++ app/src/main/java/com/xh/ad/myfragment/News.java
@@ -0,0 +1,26 @@
+package com.xh.ad.myfragment;
+
+/**
+ * Created by Administrator on 2018/4/2.
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
